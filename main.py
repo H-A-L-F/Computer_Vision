a174c53071332c7ff166e130c3dbe780dbbb0a17
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-05-10 16:56:46 +0700

    filter appled

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import cv2 as cv
 import os
+import numpy as np
+import scipy
+from scipy.interpolate import UnivariateSpline
 
 face_cascade = cv.CascadeClassifier(cv.data.haarcascades +'haarcascade_frontalface_default.xml')
 video = cv.VideoCapture(0)
@@ -11,6 +14,52 @@ recognizer.read("training.yml")
 names = []
 for users in os.listdir("dataset"):
     names.append(users)
+
+# to show multiple image
+def gallery_image(scale, img_arr):
+    rows = len(img_arr)
+    cols = len(img_arr[0])
+    available_rows = isinstance(img_arr[0], list)
+    width = img_arr[0][0].shape[1]
+    height = img_arr[0][0].shape[0]
+    if available_rows:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if img_arr[x][y].shape[:2] == img_arr[0][0].shape [:2]:
+                    img_arr[x][y] = cv.resize(img_arr[x][y], (0, 0), None, scale, scale)
+                else:
+                    img_arr[x][y] = cv.resize(img_arr[x][y], (img_arr[0][0].shape[1], img_arr[0][0].shape[0]), None, scale, scale)
+                if len(img_arr[x][y].shape) == 2: img_arr[x][y]= cv.cvtColor(img_arr[x][y], cv.COLOR_GRAY2BGR)
+        blank_image = np.zeros((height, width, 3), np.uint8)
+        horizontal = [blank_image]*rows
+        for x in range(0, rows):
+            horizontal[x] = np.hstack(img_arr[x])
+        vertic = np.vstack(horizontal)
+    else:
+        for x in range(0, rows):
+            if img_arr[x].shape[:2] == img_arr[0].shape[:2]:
+                img_arr[x] = cv.resize(img_arr[x], (0, 0), None, scale, scale)
+            else:
+                img_arr[x] = cv.resize(img_arr[x], (img_arr[0].shape[1], img_arr[0].shape[0]), None,scale, scale)
+            if len(img_arr[x].shape) == 2: img_arr[x] = cv.cvtColor(img_arr[x], cv.COLOR_GRAY2BGR)
+        horizontal = np.hstack(img_arr)
+        vertic = horizontal
+    return vertic
+
+#defining a function
+def LookupTable(x, y):
+    spline = UnivariateSpline(x, y)
+    return spline(range(256))
+
+# custom effect
+def Summer(img):
+    increaseLookupTable = LookupTable([0, 64, 128, 256], [0, 80, 160, 256])
+    decreaseLookupTable = LookupTable([0, 64, 128, 256], [0, 50, 100, 256])
+    blue_channel, green_channel,red_channel  = cv.split(img)
+    red_channel = cv.LUT(red_channel, increaseLookupTable).astype(np.uint8)
+    blue_channel = cv.LUT(blue_channel, decreaseLookupTable).astype(np.uint8)
+    sum= cv.merge((blue_channel, green_channel, red_channel ))
+    return sum
 
 if video.isOpened():
     while True:
@@ -48,9 +97,12 @@ if video.isOpened():
                     1,
                     cv.LINE_AA,
                 )
+            
+            img_canny = cv.Canny(gray, 240, 100)
         
         # show the frame
-        cv.imshow("Face Recognition", frame)
+        image_stack = gallery_image(1, [[frame, img_canny], [Summer(frame), frame]])
+        cv.imshow("Face Recognition", image_stack)
         
         
         key = cv.waitKey(1)
--- train.py
+++ train.py
@@ -7,21 +7,19 @@ from PIL import Image
 names = []
 path = []
 
-# Get the names of all the users
+# get all names
 for users in os.listdir("dataset"):
     names.append(users)
 
-# Get the path to all the images
+# get all path
 for name in names:
     for image in os.listdir("dataset/{}".format(name)):
         path_string = os.path.join("dataset/{}".format(name), image)
         path.append(path_string)
 
-
 faces = []
 ids = []
 
-# For each image create a numpy array and add it to faces list
 for img_path in path:
     image = Image.open(img_path).convert("L")
 
@@ -33,20 +31,13 @@ for img_path in path:
     faces.append(imgNp)
     ids.append(id)
 
-# Convert the ids to numpy array and add it to ids list
 ids = np.array(ids)
 
 print("[INFO] Created faces and names Numpy Arrays")
 print("[INFO] Initializing the Classifier")
 
-# Make sure contrib is installed
-# The command is pip install opencv-contrib-python
-
-# Call the recognizer
 trainer = cv2.face.LBPHFaceRecognizer_create()
-# Give the faces and ids numpy arrays
 trainer.train(faces, ids)
-# Write the generated model to a yml file
 trainer.write("training.yml")
 
 print("[INFO] Training Done")
